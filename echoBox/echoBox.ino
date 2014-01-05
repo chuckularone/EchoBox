1d869e97f532121c670ef86f64ae4bdcb4ccdd75
chuckularone <github.com@chuckularone.com>
2014-01-05 15:25:31 -0500

    Added config for PING((

--- echoBox/echoBox.ino
+++ echoBox/echoBox.ino
@@ -1,13 +1,19 @@
 /*
- outputSwitching
- 
- 
+ echoBox
+   This is a sketch that controls the control box for my desk at work.
+   It is a motorized desk that requires you to hold the button until 
+   it gets to the correct height. 
+   
+   This will allow me to program a height and press a button to get the 
+   desk to the correct location.
  */
 
-// constants won't change. They're used here to 
 // set pin numbers:
+#include <SoftwareSerial.h>
 const int outputUp = 2;
 const int outputDn = 3;
+const int trigger  = 6;
+const int pingPin  = 7;
 const int buttonUp = 8;
 const int buttonDn = 9;
 
@@ -21,10 +27,13 @@ void setup() {
   pinMode(outputDn, OUTPUT);     
   pinMode(buttonUp, INPUT);     
   pinMode(buttonDn, INPUT);     
-  
+  Serial.begin(9600);
 }
 
 void loop(){
+  long duration, inches, debug;
+  debug = 1;
+  
   setUpState = digitalRead(buttonUp);
   setDnState = digitalRead(buttonDn);
   if (setDnState == LOW) {
