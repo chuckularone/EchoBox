db19cb83ff763db7603f4260c3ebd46225072e46
Chuck McKenna <github.com@chuckularone.com>
2014-01-20 10:39:41 -0500

    Added height code

--- echoBox/echoBox.ino
+++ echoBox/echoBox.ino
@@ -1,85 +1,66 @@
 /*
  echoBox
    This is a sketch that controls the control box for my desk at work.
-   It is a motorized desk that requires you to hold the button until 
-   it gets to the correct height. 
+   It is a motorized desk that requires you to hold the button until
+   it gets to the correct height.
    
-   This will allow me to program a height and press a button to get the 
+   This will allow me to program a height and press a button to get the
    desk to the correct location.
  */
-
+ 
 // set pin numbers:
 #include <SoftwareSerial.h>
-const int outputUp = 2;
-const int outputDn = 3;
+const int outputDn = 2;
+const int outputUp = 3;
 const int trigger  = 6;
 const int pingPin  = 7;
-const int buttonUp = 8;
-const int buttonDn = 9;
-const int highInch = 36;
-const int lowInch  = 24;
-
+const int buttonDn = 8;
+const int buttonUp = 9;
+ 
 // variables will change:
 int setUpState = 0;
 int setDnState = 0;
-
+ 
 void setup() {
   // initialize the pushbutton pin as an input:
-  pinMode(outputUp, OUTPUT);     
-  pinMode(outputDn, OUTPUT);     
-  pinMode(buttonUp, INPUT);     
-  pinMode(buttonDn, INPUT);     
+  pinMode(outputUp, OUTPUT);    
+  pinMode(outputDn, OUTPUT);    
+  pinMode(buttonUp, INPUT);    
+  pinMode(buttonDn, INPUT);    
   Serial.begin(9600);
 }
-
+ 
 void loop(){
-  long duration, inches, debug;
+  long duration, debug;
+  float inches;
   debug = 1;
-  
+ 
   setUpState = digitalRead(buttonUp);
   setDnState = digitalRead(buttonDn);
-  inches = microsecondsToInches(duration);
-  
   if (setDnState == LOW) {
-    while (height > lowInch) {
-      pinMode(pingPin, OUTPUT);
-      digitalWrite(trigger, LOW);
-      delayMicroseconds(2);
-      digitalWrite(trigger, HIGH);
-      delayMicroseconds(5);
-      digitalWrite(trigger, LOW);
-      pinMode(pingPin, INPUT);
-      duration = pulseIn(pingPin, HIGH);
-      height = microsecondsToInches(duration);
-      \\ Go down!
+    duration = 10000;
+    while (duration > 2900) {
       digitalWrite(outputDn, HIGH);  
-    }
-    \\ Stop going down!
-    digitalWrite(outputDn, LOW); 
-  }
-
-
-  if (setUpState == LOW) {
-    while (height < highInch) {
+      delay(100);
       pinMode(pingPin, OUTPUT);
       digitalWrite(trigger, LOW);
       delayMicroseconds(2);
       digitalWrite(trigger, HIGH);
       delayMicroseconds(5);
       digitalWrite(trigger, LOW);
+ 
       pinMode(pingPin, INPUT);
       duration = pulseIn(pingPin, HIGH);
-      height = microsecondsToInches(duration);
-      \\ Go up!
-      digitalWrite(outputUp, HIGH);  
+      inches = microsecondsToInches(duration);
+      if (debug = 1) {
+        Serial.print(inches);
+        Serial.print("in - ");
+        Serial.print(duration);
+        Serial.print("ms");
+        Serial.println();
+      }
     }
-    \\ Stop going up!
-    digitalWrite(outputUp, LOW); 
+    digitalWrite(outputDn, LOW);
   }
-  
-}
-
-long microsecondsToInches(long microseconds)
-{
-  return microseconds / 74 / 2;
-}
+  if (setUpState == LOW) {
+    duration = 100;
